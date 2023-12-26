b764b31552494bebd51b55aef349dab7b59c5905
Nikolay Skorokhod <mrsuperpuper016@gmail.com>
2023-12-26 20:49:27 +0300

    step_2_first_project

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+def welcome_brain_games():
+    print(f'Welcome to Brain Games!')
+
+def main():
+    welcome_brain_games()
+
+if __name__ == '__main__':
+    welcome_brain_games()
