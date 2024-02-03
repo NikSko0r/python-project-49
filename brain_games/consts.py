2743a9823e5e31abdb0fb2f58b181a58adfcc646
Nikolay Skorokhod <mrsuperpuper016@gmail.com>
2024-02-03 23:26:43 +0300

    change3

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -3,7 +3,7 @@ CALC_INSTRUCTION = 'What is the result of the expression?'
 GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
 PROGR_INSTRUCTION = 'What number is missing in the progression?'
 PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. \
-    Otherwise answer "no".'
+Otherwise answer "no".'
 
 MATH_SIGNS = ('+', '-', '*')
 
