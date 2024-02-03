4dab6e433cc9beed661631125cd75672c0fa5b4a
Nikolay Skorokhod <mrsuperpuper016@gmail.com>
2024-02-03 23:22:16 +0300

    change2

--- brain_games/games/progr.py
+++ brain_games/games/progr.py
@@ -6,11 +6,11 @@ from brain_games.engine import run_game
 
 def get_progr_and_miss_num():
     start, step = random.randint(1, 100), random.randint(1, 100)
-    progr_length = random.randint(MIN_PROGR_LEN, MAX_PROGR_LEN)
-    miss_index = random.randint(0, progr_length - 1)
+    row_len = random.randint(MIN_PROGR_LEN, MAX_PROGR_LEN)
+    miss = random.randint(0, row_len - 1)  # index
     row_progr = ' '.join(
-        ['..' if i == miss_index else str(start + step * i) for i in range(progr_length)])
-    miss_num = start + step * miss_index
+        ['..' if i == miss else str(start + step * i) for i in range(row_len)])
+    miss_num = start + step * miss
     return row_progr, str(miss_num)
 
 
