94759f131fdce7094d875d55e717178d9d875d23
Nikolay Skorokhod <mrsuperpuper016@gmail.com>
2024-02-23 23:03:00 +0300

    changes_review3_1

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -2,13 +2,17 @@ import random
 import operator
 
 
-INSTRUCTION = 'What is the result of the expression?'
+INSTRUCTION = "What is the result of the expression?"
 
 
 def get_question_and_answer():
-    signs_and_operators = [('+', operator.add), ('-', operator.sub), ('*', operator.mul)]
+    signs_and_operators = [
+        ("+", operator.add),
+        ("-", operator.sub),
+        ("*", operator.mul),
+    ]
     num1, num2 = random.randint(1, 100), random.randint(1, 100)
     math_sign = random.choice(signs_and_operators)
-    question = f'{num1} {math_sign[0]} {num2}'
+    question = f"{num1} {math_sign[0]} {num2}"
     answer = math_sign[1](num1, num2)
     return question, str(answer)
